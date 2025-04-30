79e28fca28ee25fa7965d389abd13b680aca403e
sadeepa <sadeepachathushka9@gmail.com>
2025-04-30 22:58:48 +0530

    add routemidleware
    complete route and routermiddleware

--- api/routes/adminRoute.js
+++ api/routes/adminRoute.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { registerAdmin, loginAdmin } = require('../controllers/adminController');
+const { protectAdmin } = require('../middleware/authMiddleware');
 
-// Admin Register
+// Public routes
 router.post('/register', registerAdmin);
-
-// Admin Login
 router.post('/login', loginAdmin);
+
+// Protected route example
+router.get('/dashboard', protectAdmin, (req, res) => {
+  res.json({ message: `Welcome Admin ${req.admin.username}` });
+});
 
 module.exports = router;
