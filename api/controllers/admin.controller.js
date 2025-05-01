a975b6f0a9602fd8affb1687fae46d7ca84f0f59
sadeepa <sadeepachathushka9@gmail.com>
2025-05-01 11:01:26 +0530

    Update admin.controller.js
    update the admin.controller.js file

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -1,51 +1,42 @@
-const Admin = require('../models/adminModel');
-const jwt = require('jsonwebtoken');
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-// Generate token
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
-};
-
-// @desc    Register admin
-exports.registerAdmin = async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    const existingAdmin = await Admin.findOne({ email });
-    if (existingAdmin) return res.status(400).json({ message: 'Admin already exists' });
-
-    const admin = await Admin.create({ username, email, password });
-
-    res.status(201).json({
-      _id: admin._id,
-      username: admin.username,
-      email: admin.email,
-      token: generateToken(admin._id),
-    });
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+export const assignPosition = async (req, res, next) => {
+  // Only admin can access this endpoint
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "Only admin can assign positions"));
   }
-};
 
-// @desc    Login admin
-exports.loginAdmin = async (req, res) => {
-  const { email, password } = req.body;
+  const { userId, position } = req.body;
+
+  // Validate input
+  if (!userId || !position) {
+    return next(errorHandler(400, "User ID and position are required"));
+  }
+
+  // Validate position value
+  const validPositions = ['inventory_manager', 'supplier'];
+  if (!validPositions.includes(position)) {
+    return next(errorHandler(400, "Invalid position"));
+  }
+
   try {
-    const admin = await Admin.findOne({ email });
-    if (admin && (await admin.matchPassword(password))) {
-      res.json({
-        _id: admin._id,
-        username: admin.username,
-        email: admin.email,
-        token: generateToken(admin._id),
-      });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+    // Find user and update position
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { position },
+      { new: true } // Return the updated user
+    );
+
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
     }
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+
+    // Remove sensitive data before sending response
+    const { password, ...rest } = user._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
-
-// Add more admin actions (like manage users) here
