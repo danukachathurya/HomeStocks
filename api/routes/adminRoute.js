ebf4acb7834356e1f98916ce21b277a0d60fc99a
sadeepa <sadeepachathushka9@gmail.com>
2025-05-01 12:09:36 +0530

    admin  update files
    reset and fix errors

--- api/routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { assignPosition } from "../controllers/admin.controller.js";
-import { verifyToken } from "../utils/verifyUser.js";
-
-const router = express.Router();
-
-router.post("/assign-position", verifyToken, assignPosition);
-
-export default router;
